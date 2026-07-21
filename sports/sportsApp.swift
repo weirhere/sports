671ffc148b1c621afec80afd56d7f5d16e67cf69
Andy Weir <99971424+weirhere@users.noreply.github.com>
2026-07-20 20:31:32 -0400

    Initial Commit

---
+++ sports/sportsApp.swift
@@ -0,0 +1,17 @@
+//
+//  sportsApp.swift
+//  sports
+//
+//  Created by Andy Weir on 7/20/26.
+//
+
+import SwiftUI
+
+@main
+struct sportsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sportsTests/sportsTests.swift
@@ -0,0 +1,16 @@
+//
+//  sportsTests.swift
+//  sportsTests
+//
+//  Created by Andy Weir on 7/20/26.
+//
+
+import Testing
+
+struct sportsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
